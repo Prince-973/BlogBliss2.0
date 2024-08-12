4ffdd5ab49ec9354103008a370580b49fd530ae3
Prince <prinsvaghasiya.it21@scet.ac.in>
2024-08-12 17:40:34 +0530

    add button and input filed to component

--- BlogBliss 2.0/src/components/Header/Header.jsx
+++ BlogBliss 2.0/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 function Header() {
   const authStatus = useSelector((state) => state.auth.status);
   const navigate = useNavigate();
-  const navItem = [
+  const navItems = [
     {
       name: "Home",
       slug: "/",
@@ -42,6 +42,25 @@ function Header() {
               <Logo width="70px" />
             </Link>
           </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogOut />
+              </li>
+            )}
+          </ul>
         </nav>
       </Container>
     </header>
