f18997e2bf09df6edbe8f1846c0bebf7e1bed320
Prince <prinsvaghasiya.it21@scet.ac.in>
2024-08-12 14:02:00 +0530

    add the LogOut and Fotter and Header pages and its functionallity

--- BlogBliss 2.0/src/components/index.js
+++ BlogBliss 2.0/src/components/index.js
@@ -1,4 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
-export { Header, Footer };
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogOut from "./Header/LogOut";
+export { Header, Footer, Container, Logo, LogOut };
