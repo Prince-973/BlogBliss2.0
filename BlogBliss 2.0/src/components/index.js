1cd06ff72543af6e1f72778d952c04acde51d454
Prince <prinsvaghasiya.it21@scet.ac.in>
2024-08-12 20:24:23 +0530

    complete sigup and login componante

--- BlogBliss 2.0/src/components/index.js
+++ BlogBliss 2.0/src/components/index.js
@@ -3,4 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogOut from "./Header/LogOut";
-export { Header, Footer, Container, Logo, LogOut };
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+export { Header, Footer, Container, Logo, LogOut, Select, Button, Input };
